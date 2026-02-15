8468df740dc2c7fbc26e46c75ac46ee49fc5a29e
cimo <simone.dagostino@gmail.com>
2026-02-15 22:53:32 +0900

    update

--- script/command2.sh
+++ script/command2.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-clamdscan "${1}"
+p1=$(printf '%s' "${1}" | xargs)
+
+if [ -z "${p1}" ]
+then
+    echo "command2.sh - Missing parameter."
+
+    exit 1
+fi
+
+parameter1="${1}"
+
+clamdscan "${parameter1}"
