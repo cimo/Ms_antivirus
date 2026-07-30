afdde53cc4ba94c5c43cefc5b3bbb4c051496a08
cimo <simone.dagostino@gmail.com>
2026-07-31 00:03:40 +0900

    update

--- script/command2.sh
+++ script/command2.sh
@@ -2,13 +2,13 @@
 
 p1=$(printf '%s' "${1}" | xargs)
 
-if [ "$#" -lt 1 ]
+if [ "$#" -lt 1 ] || [ -z "${p1}" ]
 then
     echo -e "\n❌ command2.sh - Missing parameter."
 
     exit 1
 fi
 
-parameter1="${1}"
+parameter1="${p1}"
 
 clamdscan "${parameter1}"
