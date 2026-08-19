fbdcecbc4840f41e85940ca81d1045eef152997e
simone.dagostino <simone.dagostino@jp.kpmg.com>
2026-08-19 21:53:27 +0900

    update

--- src/HelperSrc.ts
+++ src/HelperSrc.ts
@@ -187,6 +187,30 @@ export const jsonCheck = (value: string): boolean => {
     }
 };
 
+export const fileCheckMimeType = (value: string): boolean => {
+    if (!MIME_TYPE.includes(value)) {
+        return false;
+    }
+
+    return true;
+};
+
+export const fileCheckSize = (byte: number): boolean => {
+    const fileSizeMb = parseInt(FILE_SIZE_MB);
+
+    if (isNaN(fileSizeMb)) {
+        return false;
+    }
+
+    const maxSizeByte = fileSizeMb * 1024 * 1024;
+
+    if (byte > maxSizeByte) {
+        return false;
+    }
+
+    return true;
+};
+
 export const fileDetail = async (value: string, buffer?: Uint8Array, isOnlyByte = true): Promise<modelHelperSrc.IfileDetail> => {
     let resultObject = {} as modelHelperSrc.IfileDetail;
 
@@ -385,7 +409,7 @@ export const fileDetail = async (value: string, buffer?: Uint8Array, isOnlyByte 
             if (isMatched) {
                 resultObject = {
                     ...resultObject,
-                    fileName: fileNameWithExtension,
+                    name: fileNameWithExtension,
                     baseName,
                     mimeType: signatureList[a].mimeType,
                     extension: signatureList[a].extension,
@@ -404,7 +428,7 @@ export const fileDetail = async (value: string, buffer?: Uint8Array, isOnlyByte 
             if (signatureList[a].extension === extension) {
                 resultObject = {
                     ...resultObject,
-                    fileName: fileNameWithExtension,
+                    name: fileNameWithExtension,
                     baseName,
                     mimeType: signatureList[a].mimeType,
                     extension: signatureList[a].extension,
@@ -417,30 +441,6 @@ export const fileDetail = async (value: string, buffer?: Uint8Array, isOnlyByte 
     }
 
     return resultObject;
-};
-
-export const fileCheckMimeType = (value: string): boolean => {
-    if (!MIME_TYPE.includes(value)) {
-        return false;
-    }
-
-    return true;
-};
-
-export const fileCheckSize = (byte: number): boolean => {
-    const fileSizeMb = parseInt(FILE_SIZE_MB);
-
-    if (isNaN(fileSizeMb)) {
-        return false;
-    }
-
-    const maxSizeByte = fileSizeMb * 1024 * 1024;
-
-    if (byte > maxSizeByte) {
-        return false;
-    }
-
-    return true;
 };
 
 export const fileWriteStream = (pathFile: string, buffer: Buffer): Promise<boolean | NodeJS.ErrnoException> => {
@@ -532,6 +532,37 @@ export const fileOrFolderMove = (pathCurrent: string, pathTarget: string): Promi
             }
 
             resolve(true);
+        });
+    });
+};
+
+export const fileOrFolderRename = (
+    pathOld: string,
+    pathNew: string,
+    fileNameOld?: string,
+    fileNameNew?: string
+): Promise<boolean | NodeJS.ErrnoException> => {
+    return new Promise((resolve) => {
+        Fs.rename(pathOld, pathNew, (error) => {
+            if (error) {
+                resolve(error);
+
+                return;
+            }
+
+            if (fileNameOld === undefined || fileNameNew === undefined) {
+                resolve(true);
+            } else {
+                Fs.rename(`${pathNew}${fileNameOld}`, `${pathNew}${fileNameNew}`, (error) => {
+                    if (error) {
+                        resolve(error);
+
+                        return;
+                    }
+
+                    resolve(true);
+                });
+            }
         });
     });
 };
@@ -664,7 +695,7 @@ export const readFirstLevelRecursive = (path: string, extension: string, pathPre
                     Fs.stat(pathData, (errorStat, statData) => {
                         if (!errorStat && statData.isDirectory()) {
                             if (!pathPrevious) {
-                                resultList.push(pathData);
+                                resultList.push(`${pathData}/`);
 
                                 readFirstLevelRecursive(`${pathData}/`, extension, path).then((dataSubList) => {
                                     for (let a = 0; a < dataSubList.length; a++) {
